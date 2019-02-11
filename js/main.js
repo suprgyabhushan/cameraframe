f34f21a59057831f04e31ae0b0789cc3b4465f2e
suprgyabhushan <Suprgya.56@iiitb.org>
2019-02-11 23:50:38 +0530

    n

--- js/main.js
+++ js/main.js
@@ -64,7 +64,7 @@ function initCameraUI() {
     
     video = document.getElementById('video');
 
-    takePhotoButton = document.getElementById('takePhotoButton');
+    //takePhotoButton = document.getElementById('takePhotoButton');
     toggleFullScreenButton = document.getElementById('toggleFullScreenButton');
     switchCameraButton = document.getElementById('switchCameraButton');
     cameraSensor = document.getElementById('camerasensor');
@@ -76,10 +76,10 @@ function initCameraUI() {
     // https://developer.mozilla.org/nl/docs/Web/HTML/Element/button
     // https://developer.mozilla.org/en-US/docs/Web/Accessibility/ARIA/ARIA_Techniques/Using_the_button_role
 
-    takePhotoButton.addEventListener("click", function() {
+    /*takePhotoButton.addEventListener("click", function() {
         //takeSnapshotUI();
         takeSnapshot();        
-    });
+    });*/
 
     // -- fullscreen part
 
