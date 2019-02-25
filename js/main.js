e4a5e5506bba43f345b5b5355d782bef5f9ab3c6
suprgyabhushan <Suprgya.56@iiitb.org>
2019-02-25 09:11:21 +0530

    eyes detection

--- js/main.js
+++ js/main.js
@@ -5,9 +5,15 @@
 
 */
 
+/*import * as posenet from '@tensorflow-models/posenet';
+import dat from 'dat.gui';
+import Stats from 'stats.js';
+
+import {drawBoundingBox, drawKeypoints, drawSkeleton} from './demo_util';*/
+
+
 var takeSnapshotUI = createClickFeedbackUI();
 
-var video;
 var takePhotoButton;
 var toggleFullScreenButton;
 var switchCameraButton;
@@ -15,6 +21,10 @@ var amountOfCameras = 0;
 var currentFacingMode = 'environment';
 var cameraSensor;
 var cameraOutput;
+var video;
+
+
+
 
 
 
@@ -63,12 +73,17 @@ document.addEventListener("DOMContentLoaded", function(event) {
 function initCameraUI() {
     
     video = document.getElementById('video');
+    var videoWidth = video.width;
+    var videoHeight = video.height;
+    //net = await posenet.load(0.75);
 
     //takePhotoButton = document.getElementById('takePhotoButton');
     toggleFullScreenButton = document.getElementById('toggleFullScreenButton');
     switchCameraButton = document.getElementById('switchCameraButton');
     cameraSensor = document.getElementById('camerasensor');
     cameraOutput = document.getElementById('cameraoutput');
+
+    //detectPoseInRealTime(video, net);
 
     
     //drawloop();
@@ -301,6 +316,94 @@ function createClickFeedbackUI() {
     }
 }
 
+/*function detectPoseInRealTime(video, net) {
+  const canvas = document.getElementById('camerasensor');
+  const ctx = canvas.getContext('2d');
+  // since images are being fed from a webcam
+  const flipHorizontal = true;
 
-  
+  canvas.width = videoWidth;
+  canvas.height = videoHeight;
+
+  async function poseDetectionFrame() {
+    if (guiState.changeToArchitecture) {
+      // Important to purge variables and free up GPU memory
+      guiState.net.dispose();
+
+      // Load the PoseNet model weights for either the 0.50, 0.75, 1.00, or 1.01
+      // version
+      guiState.net = await posenet.load(+guiState.changeToArchitecture);
+
+      guiState.changeToArchitecture = null;
+    }
+
+    // Begin monitoring code for frames per second
+    //stats.begin();
+
+    // Scale an image down to a certain factor. Too large of an image will slow
+    // down the GPU
+    const imageScaleFactor = guiState.input.imageScaleFactor;
+    const outputStride = +guiState.input.outputStride;
+
+    let poses = [];
+    let minPoseConfidence;
+    let minPartConfidence;
+    switch (guiState.algorithm) {
+      case 'single-pose':
+        const pose = await guiState.net.estimateSinglePose(
+            video, imageScaleFactor, flipHorizontal, outputStride);
+        poses.push(pose);
+
+        minPoseConfidence = +guiState.singlePoseDetection.minPoseConfidence;
+        minPartConfidence = +guiState.singlePoseDetection.minPartConfidence;
+        break;
+      case 'multi-pose':
+        poses = await guiState.net.estimateMultiplePoses(
+            video, imageScaleFactor, flipHorizontal, outputStride,
+            guiState.multiPoseDetection.maxPoseDetections,
+            guiState.multiPoseDetection.minPartConfidence,
+            guiState.multiPoseDetection.nmsRadius);
+
+        minPoseConfidence = +guiState.multiPoseDetection.minPoseConfidence;
+        minPartConfidence = +guiState.multiPoseDetection.minPartConfidence;
+        break;
+    }
+
+    ctx.clearRect(0, 0, videoWidth, videoHeight);
+
+    if (guiState.output.showVideo) {
+      ctx.save();
+      ctx.scale(-1, 1);
+      ctx.translate(-videoWidth, 0);
+      ctx.drawImage(video, 0, 0, videoWidth, videoHeight);
+      ctx.restore();
+    }
+
+    // For each pose (i.e. person) detected in an image, loop through the poses
+    // and draw the resulting skeleton and keypoints if over certain confidence
+    // scores
+    poses.forEach(({score, keypoints}) => {
+      if (score >= minPoseConfidence) {
+        if (guiState.output.showPoints) {
+          drawKeypoints(keypoints, minPartConfidence, ctx);
+        }
+        if (guiState.output.showSkeleton) {
+          drawSkeleton(keypoints, minPartConfidence, ctx);
+        }
+        if (guiState.output.showBoundingBox) {
+          drawBoundingBox(keypoints, ctx);
+        }
+      }
+    });
+
+    // End monitoring code for frames per second
+    //stats.end();
+
+    requestAnimationFrame(poseDetectionFrame);
+  }
+
+  poseDetectionFrame();
+}*/
+
+
 
